9e073575a162afc6cd5554279ce105eec0409272
Serj <sergejaver@gmail.com>
2023-07-05 13:42:48 +0300

    corrected cards

--- src/Components/Molecules/Card.jsx
+++ src/Components/Molecules/Card.jsx
@@ -6,7 +6,6 @@ const Card = styled.div`
   padding: 20px;
   border-radius: 5px;
   transition: box-shadow 0.3s;
-
   &:hover {
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
 
@@ -14,7 +13,7 @@ const Card = styled.div`
       display: block;
     }
   }
-`;
+  `;
 
 const Title = styled.h3`
   font-size: 18px;
@@ -39,16 +38,45 @@ const AddToCartButton = styled.button`
   border-radius: 3px;
   cursor: pointer;
   display: none;
+  
 `;
+
+const CardM = styled.div`
+  background-color: #f9f9f9;
+  padding: 20px;
+  border-radius: 5px;
+  transition: box-shadow 0.3s;
+  
+`;
+
+const AddToCartButtonM = styled.button`
+  background-color: #333;
+  color: #fff;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+
+`;
+
+
 
 const CardDesktop = ({ product }) => {
   return (
-    <Card>
-      <Title>{product.name}</Title>
-      <Price>{product.price}</Price>
-      <TypeFood>{product.typeFood}</TypeFood>
-      <AddToCartButton>Add to Cart</AddToCartButton>
-    </Card>
+    <>
+      <Card>
+        <Title>{product.name}</Title>
+        <Price>{product.price}</Price>
+        <TypeFood>{product.typeFood}</TypeFood>
+        <AddToCartButton>Add to Cart</AddToCartButton>
+      </Card>
+      <CardM>
+        <Title>{product.name}</Title>
+        <Price>{product.price}</Price>
+        <TypeFood>{product.typeFood}</TypeFood>
+        <AddToCartButtonM>Add to Cart</AddToCartButtonM>
+      </CardM>
+    </>
   );
 };
 
