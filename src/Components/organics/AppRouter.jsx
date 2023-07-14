b28bf19cd775264601a6d338aadce49c62d93b95
Serj <sergejaver@gmail.com>
2023-07-14 21:11:44 +0300

    cart

--- src/Components/organics/AppRouter.jsx
+++ src/Components/organics/AppRouter.jsx
@@ -3,23 +3,26 @@ import Header from './Header';
 import MainGrid from '../otherPages/StyledMainGreed';
 import CartPage from './CartPage';
 import { Route, Routes } from 'react-router-dom';
+import Contacts from '../molecules/Contacts';
 
 const AppRouter = () => {
-    const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
   const addToCart = (product) => {
     setCartItems((prevItems) => [...prevItems, product]);
   };
 
 
-    return (
-        <>
-        <Header />
-        <Routes>
-          <Route path="/homepage" element={<MainGrid addToCart={addToCart} />}/>
-          <Route path="/cart" element={<CartPage cartItems={cartItems} />} />
-        </Routes>
-        </>
-    );
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/homepage" element={<MainGrid addToCart={addToCart} />}/>
+        <Route path="/cart" element={<CartPage cartItems={cartItems} />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </>
+  );
 };
 
 
