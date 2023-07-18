232fad78df049f83332c88215c186a78c1ceac91
Ihor Kubrak <ihor.kubrak@litslink.com>
2023-07-18 17:42:44 +0300

    Moved 'pages' to separeted folder, added src and pages aliases

--- src/shared/index.js
+++ src/shared/index.js
@@ -9,7 +9,3 @@ export { Logo } from './atoms/Logo'
 export { Card } from './molecules/Card'
 // Organisms
 export { Header } from './organisms/Header'
-// Pages
-export { ContactsPage } from './pages/ContactsPage'
-export { CartPage } from './pages/CartPage'
-export { HomePage } from './pages/HomePage'
