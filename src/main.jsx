8926aadd64ea9bd53d26c09a15a15c241dd6495c
Ihor Kubrak <ihor.kubrak@litslink.com>
2023-07-18 17:34:15 +0300

    Fixed imports & file names & structure, added alias 'shared'

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-//styles
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-// Sematic UI styles added
+// Styles
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/shared/atoms/buttons/AddToCartButton.jsx
+++ src/shared/atoms/buttons/AddToCartButton.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 
 
-const AddToCartButton = ({ onClick, children }) => {
+export const AddToCartButton = ({ onClick, children }) => {
   return <Button onClick={onClick}>{children}</Button>;
 };
-
-
-
-export default AddToCartButton;
